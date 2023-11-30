6b2432894c0d1700882b30c80c224d50ee75bf09
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2023-11-30 20:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+  //   write your code here 
+	let y1=new Date(date1).getTime();
+	let y2=new Date(date2).getTime();
+	let diff y2-y1;
+	return Math.floor(diff/(1000*60*60*24));
+	
 };
 
 // Do not change the code below.
